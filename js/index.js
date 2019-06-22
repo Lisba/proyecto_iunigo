8e6c80a0a380b36bfa5415e86be69aacd830415e
Lisbaldy <lisba7474@gmail.com>
2019-06-22 17:27:49 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,104 @@
+// class user{
+    
+    //     constructor(name, lastName, pts){
+        //         this.name = name
+        //         this.lastName = lastName
+        //         this.pts = pts
+//     }
+
+//     sayName(){
+    //         return `${name} ${lastName}`
+    //     }
+    
+    //     sayPts(){
+        //         return `${pts}`
+        //     }
+        
+        // }
+        
+        // let userNew = new user (name, lastName, pts)
+function enableGift (){
+
+    let button = document.getElementById('btn-iunigo')
+    button.setAttribute('disabled', 'true')
+
+    if(currentUser['pts'] < currentGift['pts']){
+        button.disabled = true
+        button.setAttribute('style', 'background-color: red;')
+    } else {
+        button.disabled = false
+    }
+}
+
+let user1 = {
+name: 'Name1',
+lastName: 'LastName1',
+pts: 100
+}
+
+let user2 = {
+    name: 'Name2',
+    lastName: 'LastName2',
+    pts: 200
+}
+
+let gift1 = {
+    id: 'btn-vtv',
+    name: `Verificación técnica vehicular`,
+    pts: 150
+}
+
+let gift2 = {
+    id: 'btn-rmf',
+    name: `Recarga de Matafuegos`,
+    pts: 100
+}
+
+let gift3 = {
+    id: 'btn-fcg',
+    name: `Funda cubregranizos`,
+    pts: 200
+}
+
+let currentUser = user1
+let currentGift = gift1
+let currentGift2 = gift2
+let currentGift3 = gift3
+
+console.log(currentGift)
+console.log(currentGift2)
+console.log(currentGift3)
+console.log(currentUser)
+
+
+function showGift(gift){
+
+    let giftIunigo1 = document.createElement('button')
+    giftIunigo1.setAttribute('class', 'btn btn-link')
+    giftIunigo1.setAttribute('type', 'button')
+    giftIunigo1.setAttribute('id', `${gift['id']}`)
+    giftIunigo1.setAttribute('data-target','#collapseOne')
+    giftIunigo1.setAttribute('aria-controls', 'collapseOne')
+    giftIunigo1.setAttribute('aria-expanded', 'true')
+    giftIunigo1.setAttribute('data-toggle', 'collapse')
+    giftIunigo1.innerHTML = `${gift['name']} ${gift['pts']}`
+    document.getElementById('h2-btn').appendChild(giftIunigo1)
+
+}
+
+function showData(user){
+
+    let userIunigo1 = document.createElement('h2')
+    userIunigo1.innerHTML = `${user['name']} ${user['lastName']}`
+    document.getElementById('userName').appendChild(userIunigo1)
+
+    let userIunigo2 = document.createElement('h2')
+    userIunigo2.innerHTML = `${user['pts']} pts`
+    document.getElementById('userPts').appendChild(userIunigo2)
+
+}
+
+showData(currentUser)
+showGift(currentGift)
+showGift(currentGift2)
+showGift(currentGift3)
