8c01063c4f10fe6b6ae0f755e32717d00ce27d6c
Lisbaldy <lisba7474@gmail.com>
2019-06-30 14:56:41 -0300

    automation with functions

--- js/index.js
+++ js/index.js
@@ -36,20 +36,20 @@ function enableGift () {
 //enableGift recorre la matriz de gifts (currentGift), luego por cada iteracion evalua si los pts del usuario son mayores o iguales a los pts requeridos por el gift a canjear ubica el boton por id (suma 1 al i y lo convierte a string para concatenarlo al id a buscar) y lo habilita.
 
 let user1 = {
-    name: 'Name1',
-    lastName: 'LastName1',
+    name: 'Lisbaldy',
+    lastName: 'Ojeda',
     pts: 100
 }
 
 let user2 = {
-    name: 'Name2',
-    lastName: 'LastName2',
+    name: 'Keimarly',
+    lastName: 'Márquez',
     pts: 200
 }
 
 let user3 = {
-    name: 'Name3',
-    lastName: 'LastName3',
+    name: 'Nazarelis',
+    lastName: 'Ojeda',
     pts: 160
 }
 
@@ -77,71 +77,87 @@ let currentGift = [gift1, gift2, gift3]
 console.log(currentGift)
 console.log(currentUser)
 
-function showData(user){
+function showData(user, idName, idPts){
+    
+    let userIunigo = document.createElement('h2')
+    userIunigo.innerHTML = `${user['name']} ${user['lastName']}`
+    document.getElementById(idName).appendChild(userIunigo)
     
     let userIunigo1 = document.createElement('h2')
-    userIunigo1.innerHTML = `${user['name']} ${user['lastName']}`
-    document.getElementById('userName').appendChild(userIunigo1)
-    
-    let userIunigo2 = document.createElement('h2')
-    userIunigo2.innerHTML = `${user['pts']} pts`
-    document.getElementById('userPts').appendChild(userIunigo2)
+    userIunigo1.innerHTML = `${user['pts']} pts`
+    document.getElementById(idPts).appendChild(userIunigo1)
     
 }
 
-function showGift(gift){
+function showGift(gift, collapsed, id, data_target, aria_controls, aria_expanded, fatherid){
     
-    let giftIunigo1 = document.createElement('button')
-    giftIunigo1.setAttribute('style', 'color: #1826c4;')
-    giftIunigo1.setAttribute('class', 'btn btn-link')
-    giftIunigo1.setAttribute('type', 'button')
-    giftIunigo1.setAttribute('id', 'btn-vtv')
-    giftIunigo1.setAttribute('data-target','#collapseOne')
-    giftIunigo1.setAttribute('aria-controls', 'collapseOne')
-    giftIunigo1.setAttribute('aria-expanded', 'true')
-    giftIunigo1.setAttribute('data-toggle', 'collapse')
-    giftIunigo1.innerHTML = `${gift['name']} - ${gift['pts']}`
-    document.getElementById('h2-btn').appendChild(giftIunigo1)
-    
-}
-
-function showGift1(gift){
-    
-    let giftIunigo2 = document.createElement('button')
-    giftIunigo2.setAttribute('style', 'color: #1826c4;')
-    giftIunigo2.setAttribute('class', "btn btn-link collapsed")
-    giftIunigo2.setAttribute('type', "button")
-    giftIunigo2.setAttribute('id', 'btn-rmf')
-    giftIunigo2.setAttribute('data-target',"#collapseTwo")
-    giftIunigo2.setAttribute('aria-controls', "collapseTwo")
-    giftIunigo2.setAttribute('aria-expanded', "false")
-    giftIunigo2.setAttribute('data-toggle', 'collapse')
-    giftIunigo2.innerHTML = `${gift['name']} - ${gift['pts']}`
-    document.getElementById('h2-btn1').appendChild(giftIunigo2)
+    let giftIunigo = document.createElement('button')
+    giftIunigo.setAttribute('style', 'color: #1826c4;')
+    giftIunigo.setAttribute('class', `btn btn-link ${collapsed}`)
+    giftIunigo.setAttribute('type', 'button')
+    giftIunigo.setAttribute('id', id)
+    giftIunigo.setAttribute('data-target', `#${data_target}`)
+    giftIunigo.setAttribute('aria-controls', aria_controls)
+    giftIunigo.setAttribute('aria-expanded', aria_expanded)
+    giftIunigo.setAttribute('data-toggle', 'collapse')
+    giftIunigo.innerHTML = `${gift['name']} - ${gift['pts']}`
+    document.getElementById(fatherid).appendChild(giftIunigo)
     
 }
 
-function showGift2(gift){
+// function showGift(gift){
     
-    let giftIunigo3 = document.createElement('button')
-    giftIunigo3.setAttribute('style', 'color: #1826c4;')
-    giftIunigo3.setAttribute('class', 'btn btn-link')
-    giftIunigo3.setAttribute('type', 'button')
-    giftIunigo3.setAttribute('id', 'btn-fcg')
-    giftIunigo3.setAttribute('data-target', "#collapseThree")
-    giftIunigo3.setAttribute('aria-controls', "collapseThree")
-    giftIunigo3.setAttribute('aria-expanded', 'false')
-    giftIunigo3.setAttribute('data-toggle', 'collapse')
-    giftIunigo3.innerHTML = `${gift['name']} - ${gift['pts']}`
-    document.getElementById('h2-btn2').appendChild(giftIunigo3)
+//     let giftIunigo1 = document.createElement('button')
+//     giftIunigo1.setAttribute('style', 'color: #1826c4;')
+//     giftIunigo1.setAttribute('class', 'btn btn-link')
+//     giftIunigo1.setAttribute('type', 'button')
+//     giftIunigo1.setAttribute('id', 'btn-vtv')
+//     giftIunigo1.setAttribute('data-target','#collapseOne')
+//     giftIunigo1.setAttribute('aria-controls', 'collapseOne')
+//     giftIunigo1.setAttribute('aria-expanded', 'true')
+//     giftIunigo1.setAttribute('data-toggle', 'collapse')
+//     giftIunigo1.innerHTML = `${gift['name']} - ${gift['pts']}`
+//     document.getElementById('h2-btn').appendChild(giftIunigo1)
     
-}
+// }
+
+// function showGift1(gift){
+    
+//     let giftIunigo2 = document.createElement('button')
+//     giftIunigo2.setAttribute('style', 'color: #1826c4;')
+//     giftIunigo2.setAttribute('class', "btn btn-link collapsed")
+//     giftIunigo2.setAttribute('type', "button")
+//     giftIunigo2.setAttribute('id', 'btn-rmf')
+//     giftIunigo2.setAttribute('data-target',"#collapseTwo")
+//     giftIunigo2.setAttribute('aria-controls', "collapseTwo")
+//     giftIunigo2.setAttribute('aria-expanded', "false")
+//     giftIunigo2.setAttribute('data-toggle', 'collapse')
+//     giftIunigo2.innerHTML = `${gift['name']} - ${gift['pts']}`
+//     document.getElementById('h2-btn1').appendChild(giftIunigo2)
+    
+// }
+
+// function showGift2(gift){
+    
+//     let giftIunigo3 = document.createElement('button')
+//     giftIunigo3.setAttribute('style', 'color: #1826c4;')
+//     giftIunigo3.setAttribute('class', 'btn btn-link')
+//     giftIunigo3.setAttribute('type', 'button')
+//     giftIunigo3.setAttribute('id', 'btn-fcg')
+//     giftIunigo3.setAttribute('data-target', "#collapseThree")
+//     giftIunigo3.setAttribute('aria-controls', "collapseThree")
+//     giftIunigo3.setAttribute('aria-expanded', 'false')
+//     giftIunigo3.setAttribute('data-toggle', 'collapse')
+//     giftIunigo3.innerHTML = `${gift['name']} - ${gift['pts']}`
+//     document.getElementById('h2-btn2').appendChild(giftIunigo3)
+    
+// }
 
 
-showData(currentUser)
-showGift(gift1)
-showGift1(gift2)
-showGift2(gift3)
+showData(currentUser, 'userName', 'userPts')
+showGift(gift1, null, 'btn-vtv', 'collapseOne', 'collapseOne', true, 'h2-btn')
+showGift(gift2, 'collapsed', 'btn-rmf', 'collapseTwo', 'collapseTwo', false, 'h2-btn1')
+showGift(gift3, 'collapsed', 'btn-fcg', 'collapseThree', 'collapseThree', false, 'h2-btn2')
 
 // let ptsCanjeados = document.createElement('h2')
 // ptsCanjeados.innerHTML = `${currentUser['pts']}`
